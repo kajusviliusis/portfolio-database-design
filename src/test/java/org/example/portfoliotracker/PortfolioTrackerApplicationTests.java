75f0f7f08fed76eddb1fe4e58c6b0921a8820849
kajusviliusis <kajus.viliusis0@gmail.com>
2026-02-24 20:11:51 +0200

    chore: initialize project

---
+++ src/test/java/org/example/portfoliotracker/PortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.portfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
